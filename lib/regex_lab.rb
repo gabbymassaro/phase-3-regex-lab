7fc49597dada178913ae9bd27ac57c088970d6cd
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-10 17:26:54 -0400

    Capital/punctuation works but I don't like the solution

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,11 +11,16 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  first_word = text.split.first
+  if first_word.match(/\A[A-Z]/) && text.match(/[.,!?;:'"]\z/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
