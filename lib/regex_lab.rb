9ec0b2da257891d6733fa49ebc30893fd230d7df
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-10 15:26:48 -0400

    Starts with vowel and un/ing complete

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A[aeiouAEIOU]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w*un\w*ing/)
 end
 
 def words_five_letters_long(text)
